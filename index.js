984926af070783d81e22e86b982148901cfce4b8
maishpie <96499709+maishpie@users.noreply.github.com>
2023-03-07 13:57:00 -0500

    testing

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
 })
 
 // Return a list of ALL movies to the user
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
     Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
